e8c4bb128a38507cf03ea3aad2342311e46d9770
Andrew <71499897+andrewhzy@users.noreply.github.com>
2024-02-26 17:33:48 +0800

    Update git.sh

--- cli/git.sh
+++ cli/git.sh
@@ -1,1 +1,15 @@
+alias gck='git checkout'
+alias gst='git status'
+alias gsh='git stash'
+alias gsha='git stash apply'
+alias gshd='git stash drop'
 
+alias gl='git log --graph --decorate'
+alias gld='git log --graph --decorate --date-order'
+alias glo='git log --graph --decorate --oneline'
+alias gldo='git log --graph --decorate --date-order --oneline'
+
+alias gla='git log --graph --decorate --all'
+alias glad='git log --graph --decorate --all --date-order'
+alias glao='git log --graph --decorate --all --oneline'
+alias glado='git log --graph --decorate --all --date-order --oneline'
